6ef8545e22b4d2e2b4b13ef7233ca82f90590e14
Will Squires <w.r.squires@hotmail.co.uk>
2022-09-15 16:21:28 +0100

    Second Commit

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -4,60 +4,66 @@ import com.opencsv.exceptions.CsvException;
 import org.apache.commons.text.WordUtils;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     static ArrayList<Person> people = new ArrayList<>();
     static Boolean running = true;
-    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
+    public static Logger LOGGER = LogManager.getLogger(Main.class.getName());
 
-    public static void main(String[] args) throws IOException, CsvException {
-        LOGGER.info("Program Launched");
-        CSVReader reader = new CSVReader(new FileReader("Transactions2014.csv"));
-        LOGGER.info("Opening Transactions2014.csv");
-        CSVReader read2 = new CSVReader(new FileReader("DodgyTransactions2015.csv"));
-        LOGGER.info("Opening DogdyTransactions2015.csv");
-        List<String[]> dodgeCsv = read2.readAll();
-        List<String[]> normCsv = reader.readAll();
-        List<String[]> csv = Stream.concat(normCsv.stream(), dodgeCsv.stream()).collect(Collectors.toList());
-        for (int i = 1; i <= csv.size()-1; i++) {
-            String record = Arrays.toString(csv.get(i));
-            String[] splitRecord = record.split(",", 5);
-            String name = splitRecord[1].substring(1);
-            if (name.equals("From")){
-                LOGGER.warn("Header parsed!");
-            } else if (people.size() == 0) {
-                people.add(new Person(name));
-            } else {
-                for (int j = 0; j <= people.size(); j++) {
-                    if (name.equals(people.get(j).name)) {
-                        break;
-                    } else if (j == people.size()-1) {
-                        people.add(new Person(name));
-                        break;
+    public static void main(String[] args){
+        try {
+            LOGGER.info("Program Launched");
+            CSVReader reader = new CSVReader(new FileReader("Transactions2014.csv"));
+            LOGGER.info("Opening Transactions2014.csv");
+            CSVReader read2 = new CSVReader(new FileReader("DodgyTransactions2015.csv"));
+            LOGGER.info("Opening DogdyTransactions2015.csv");
+            List<String[]> dodgeCsv = read2.readAll();
+            List<String[]> csv = reader.readAll();
+            csv.addAll(dodgeCsv);
+            //List<String[]> csv = Stream.concat(normCsv.stream(), dodgeCsv.stream()).collect(Collectors.toList());
+            for (int i = 1; i <= csv.size() - 1; i++) {
+                String record = Arrays.toString(csv.get(i));
+                String[] splitRecord = record.split(",", 5);
+                String name = splitRecord[1].substring(1);
+                if (name.equals("From")) {
+                    LOGGER.warn("Header parsed!");
+                } else if (people.size() == 0) {
+                    people.add(new Person(name, LOGGER));
+                } else {
+                    for (int j = 0; j <= people.size(); j++) {
+                        if (name.equals(people.get(j).name)) {
+                            break;
+                        } else if (j == people.size() - 1) {
+                            people.add(new Person(name, LOGGER));
+                            break;
+                        }
                     }
                 }
             }
+        } catch (Exception e) {
+            LOGGER.error("Processing data - " + e);
         }
-        while (running = true){
-            System.out.println("\nReady for request");
-            Scanner input = new Scanner(System.in);
-            String text = input.nextLine();
-            text = text.toUpperCase();
-            if (text.equals("LIST ALL")){
-                listAll();
-            } else if (text.startsWith("LIST")){
-                text = text.replace("LIST", "");
-                text = text.toLowerCase();
-                text = WordUtils.capitalizeFully(text).substring(1);
-                System.out.println(text);
-                list(text);
+        try {
+            while (running = true) {
+                System.out.println("\nReady for request");
+                Scanner input = new Scanner(System.in);
+                String text = input.nextLine();
+                text = text.toUpperCase();
+                if (text.equals("LIST ALL")) {
+                    listAll();
+                } else if (text.startsWith("LIST")) {
+                    text = text.replace("LIST", "");
+                    text = text.toLowerCase();
+                    text = WordUtils.capitalizeFully(text).substring(1);
+                    System.out.println(text);
+                    list(text);
+                }
             }
+        } catch(Exception e){
+            LOGGER.error("Processing data during request - " + e);
         }
     }
     public static void listAll(){
--- src/main/java/training/supportbank/Person.java
+++ src/main/java/training/supportbank/Person.java
@@ -7,6 +7,8 @@ import java.math.BigDecimal;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Person {
     String name;
@@ -14,20 +16,26 @@ public class Person {
     ArrayList<Transaction> transactions = new ArrayList<>();
     DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH);
 
-    public Person(String name) throws IOException, CsvException {
+    public Person(String name, Logger LOGGER) throws IOException, CsvException {
         System.out.println("\nCreating person..."+ name);
         this.name = name;
         CSVReader reader = new CSVReader(new FileReader("Transactions2014.csv"));
+        CSVReader read2 = new CSVReader(new FileReader("DodgyTransactions2015.csv"));
+        List<String[]> dodgeCsv = read2.readAll();
         List<String[]> csv = reader.readAll();
+        for (int j =1; j <= dodgeCsv.size()-1; j++){
+            csv.add(dodgeCsv.get(j));
+        }
         for (int i = 1; i <= csv.size()-1; i++) {
-            String record = Arrays.toString(csv.get(i));
-            String[] splitRecord = record.split(",", 5);
-            LocalDate date = LocalDate.parse(splitRecord[0].substring(1), format);
-            String from = splitRecord[1].substring(1);
-            String to = splitRecord[2].substring(1);
-            String reason = splitRecord[3].substring(1);
-            String amount = splitRecord[4].substring(1).substring(0, splitRecord[4].length()-2);
-            BigDecimal tAmount = new BigDecimal(amount);
+            try {
+                String record = Arrays.toString(csv.get(i));
+                String[] splitRecord = record.split(",", 5);
+                LocalDate date = LocalDate.parse(splitRecord[0].substring(1), format);
+                String from = splitRecord[1].substring(1);
+                String to = splitRecord[2].substring(1);
+                String reason = splitRecord[3].substring(1);
+                String amount = splitRecord[4].substring(1).substring(0, splitRecord[4].length() - 2);
+                BigDecimal tAmount = new BigDecimal(amount);
                 if (from.equals(this.name)) {
                     transactions.add(new Transaction(tAmount, reason, date, to));
                     this.total = this.total.add(tAmount);
@@ -35,7 +43,11 @@ public class Person {
                     transactions.add(new Transaction(tAmount, reason, date, from));
                     this.total = this.total.subtract(tAmount);
                 }
+            } catch(Exception e){
+                LOGGER.error("Creating transaction - " + e);
+                transactions.remove(transactions.size()-1);
             }
+        }
         System.out.println("\n" + this.name + " - Person created");
     }
 
